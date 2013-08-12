e560e8ccb450a50e9f541fd0bf5075741b119cd9
王雷 <wanglei@chinacache.com>
2013-08-12 12:08:42 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minsheng::Application.config.secret_token = 'f0e0d369f851e3b77412a623ad0b04af5d6e5fb71e235e1978f13a7e673c0a71fa3213a7f2d2613e8a51b1423b3860826a5f1430c63149b8e05957328239015f'
