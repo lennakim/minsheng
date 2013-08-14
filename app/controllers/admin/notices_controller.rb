efc43b498e7aee5f5b08bb0569616a0e9459e81d
zongyannan <zongyannan@shengtianbo.com>
2013-08-14 16:36:18 +0800

    公告实现

--- app/controllers/admin/notices_controller.rb
+++ app/controllers/admin/notices_controller.rb
@@ -1,8 +1,9 @@
 # encoding: utf-8
 
 class Admin::NoticesController < ApplicationController
-  #before_filter :authenticate_user!
+  before_filter :authenticate_user!
   before_filter :set_notice, only: [:show, :edit, :update, :destroy]
+  layout 'admin'
 
   def index
   	@notices = Notice.all
@@ -13,10 +14,13 @@ class Admin::NoticesController < ApplicationController
   end
 
   def create
-    if Notice.create(params[:notice])
+    issue_time = params[:notice][:issue_time]
+    params[:notice][:issue_time] = issue_time.to_time unless issue_time.blank?
+    @notice = Notice.new(params[:notice])
+    if @notice.save
       redirect_to admin_notices_path
     else
-      flash[:notice] = 'error'
+      flash[:notice] = '不能为空'
       render 'new'
     end
   end
@@ -28,17 +32,19 @@ class Admin::NoticesController < ApplicationController
   end
 
   def update
+    issue_time = params[:notice][:issue_time]
+    params[:notice][:issue_time] = issue_time.to_time unless issue_time.blank?
     if @notice.update_attributes(params[:notice])
       redirect_to admin_notices_path
     else
-      flash[:notice] = 'error'
+      flash[:notice] = '不能为空'
       render 'edit'
     end
   end
 
   def destroy
     @notice.destroy
-    render json: {reload: true}
+    redirect_to admin_notices_path
   end
 
   private
--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,7 +1,17 @@
 # encoding: utf-8
 
 class Notice < ActiveRecord::Base
-  attr_accessible :content, :tag_type, :title
+  attr_accessible :content, :tag_type, :title, :issue_time
+
+  validates :content, :tag_type, :title, :issue_time, presence: true
 
   TAG_TYPE = { '前' => 1, '后' => 2, '左' => 3, '右' => 4 }
+
+  def format_date(format_str = '%Y-%m-%d %H:%M:%S')
+    issue_time.nil? ? nil : issue_time.strftime(format_str)
+  end
+
+  def tag_text
+    tag_type.nil? ? nil : TAG_TYPE.key(tag_type.to_i)
+  end
 end
