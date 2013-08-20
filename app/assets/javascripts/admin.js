c59a834bb253acd5e21ec69675d032638f6a73b5
王雷 <wanglei@chinacache.com>
2013-08-20 15:26:16 +0800

    added admin template

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,31 @@
+
+
+//= require admin/jquery
+//= require admin/bootstrap
+//= require admin/jquery-ui-1.9.2.custom.min
+//= require admin/fullcalendar.min
+//= require admin/jquery.rateit.min
+//= require admin/jquery.prettyPhoto
+// jQuery Flot
+//= require admin/excanvas.min
+//= require admin/jquery.flot
+//= require admin/jquery.flot.resize
+//= require admin/jquery.flot.pie
+//= require admin/jquery.flot.stack
+// jQuery Notification - Noty
+//= require admin/jquery.noty
+//= require admin/themes/default
+//= require admin/layouts/bottom
+//= require admin/layouts/topRight
+//= require admin/layouts/top
+
+//= require admin/sparklines
+//= require admin/jquery.cleditor.min
+//= require admin/bootstrap-datetimepicker.min
+//= require admin/jquery.uniform.min
+//= require admin/jquery.toggle.buttons
+//= require admin/filter
+//= require admin/custom
+//= require admin/charts
+
+
