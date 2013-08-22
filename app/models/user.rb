0fe1411899d88a99a6a6801e492318841b95c7fd
王雷 <wanglei@chinacache.com>
2013-08-22 17:21:36 +0800

    added user seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   #, :token_authenticatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :confirmed_at
 
   has_many :rates, dependent: :destroy
 end
