18d0c58027df954839df99dfc62b000ea09ce8c4
shooterman <shooter.hero@gmail.com>
2013-08-26 19:26:03 +0800

    添加权限管理

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,31 +2,43 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    account_modify_authority
+    account_rw_authority
+
     user ||= User.new # guest user (not logged in)
     if user.has_role? :admin
       can :manage, :all
+    elsif user.has_role? :editor
+      editor_can
+    else
+      guest_can
     end
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user permission to do.
-    # If you pass :manage it will apply to every action. Other common actions here are
-    # :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on. If you pass
-    # :all it will apply to every resource. Otherwise pass a Ruby class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details: https://github.com/ryanb/cancan/wiki/Defining-Abilities
+  end
+
+  def editor_can
+    cannot [:edit_role, :update_role, :index], User
+
+    can :rw, User
+    can :modify, User do |user|
+      user == user
+    end
+
+    can :rw, Rate
+    can :modify, Rate do |rate|
+      rate.user == user
+    end
+
+  end
+
+  def guest_can
+     cannot :manage, :all
+  end
+
+  def account_modify_authority
+    alias_action :edit, :update, :destroy, :to => :modify
+  end
+
+  def account_rw_authority
+    alias_action :index, :show, :new, :create, :to => :rw
   end
 end
