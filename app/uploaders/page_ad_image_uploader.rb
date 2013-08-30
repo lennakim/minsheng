c69ac4feac7434569ccf6b4c234ec5219a905709
lijg <fantaxy025025@126.com>
2013-08-30 20:08:22 +0800

    perfect image read

--- app/uploaders/page_ad_image_uploader.rb
+++ app/uploaders/page_ad_image_uploader.rb
@@ -2,7 +2,7 @@ class PageAdImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
   # include Sprockets::Helpers::RailsHelper
@@ -12,23 +12,21 @@ class PageAdImageUploader < CarrierWave::Uploader::Base
   # storage :file
   # storage :fog
   storage :grid_fs
-
   def root
     Rails.root.join('tmp')
   end
-
   def cache_dir
     "uploads"
   end
-
   def grid_fs_access_url
-    "/images"
+    "/images/upload"
   end
+
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -47,20 +45,20 @@ class PageAdImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+  #version :thumb do
+  #  process :resize_to_fill => [50, 50]
+  #end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
-     'o.jpg' if original_filename
+    'o.jpg' if original_filename
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Minsheng::Application.routes.draw do
   # resources :products
 
 
-  match "/images/upload/:model/url/:id/:filename" => "gridfs#serve"
+  match "/images/upload/:model/:field_name/:id/:filename" => "gridfs#serve"
 
 
   match "admin/cities" => "admin/communities#cities"
