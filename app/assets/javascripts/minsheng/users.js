5c0a494880b135eaebb37231ee0d6ff64af37f01
zongyannan <zongyannan@shengtianbo.com>
2013-09-04 11:40:01 +0800

    头像处理

--- app/assets/javascripts/minsheng/users.js
+++ app/assets/javascripts/minsheng/users.js
@@ -1,47 +1,26 @@
 $("body.users.upload_image").ready(function(){
   $("#user_image_url").change(function(){
-    $this = this;
-    var jcrop_api, boundx, boundy;
-    if ($this.files && $this.files[0]) {
+    if (this.files && this.files[0]) {
       reader = new FileReader();
-      reader.onload = function (e) {
-        $('#show_image').attr('src', e.target.result);
-        $('#preview').attr('src', e.target.result);
-        $('#show_image').attr('style', '');
-        $('#show_image').removeData('Jcrop');
+      reader.onload = function (event) {
+        $('#preview').attr('src', event.target.result);
         $('.jcrop-holder').remove();
-        $("#show_image").Jcrop({
-          setSelect:[0, 0, 100, 100],
-          minSize: [100, 100],
-          maxSize: [300, 300],
-          allowSelect: false,
-          onSelect: updatePreview,
-          aspectRatio:1
-        },function(){
-          bounds=this.getBounds();
-          boundx=bounds[0];
-          boundy=bounds[1];
-          jcrop_api=this;
-        });
+        $('#show_image').attr({'style': '', 'src': event.target.result}).removeData('Jcrop').
+          Jcrop({
+            setSelect:[0, 0, 100, 100],
+            minSize: [100, 100],
+            maxSize: [300, 300],
+            allowSelect: false,
+            aspectRatio: 1,
+            onSelect: function(coords){
+              $('#top').val(coords.x);
+              $('#left').val(coords.y);
+              $('#width').val(coords.w);
+              $('#height').val(coords.h);
+            }
+          });
       };
-      reader.readAsDataURL($this.files[0]);
-    };
-    function updatePreview(c){
-      $('#top').val(c.x);
-      $('#left').val(c.y);
-      $('#width').val(c.w);
-      $('#height').val(c.h);
-
-      if(parseInt(c.w)>0){
-        var rx=100/c.w;
-        var ry=100/c.h;
-        $("#preview").css({
-          width:Math.round(rx*boundx)+"px",
-          height:Math.round(ry*boundy)+"px",
-          marginLeft:"-"+Math.round(rx*c.x)+"px",
-          marginTop:"-"+Math.round(ry*c.y)+"px"
-        });
-      };
+      reader.readAsDataURL(this.files[0]);
     };
   });
 });
