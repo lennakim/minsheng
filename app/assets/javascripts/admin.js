1c0c1fdf8889309e5039261e4dcabe5e8f333e36
lijg <fantaxy025025@126.com>
2013-09-04 19:57:08 +0800

    修正js和css的存放位置

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,44 +1,12 @@
-
-
 //= require admin/jquery
-//= require admin/bootstrap
+//= require lib/bootstrap
 //= require admin/jquery-ui-1.9.2.custom.min
-//= require admin/fullcalendar.min
-//= require admin/jquery.rateit.min
-//= require admin/jquery.prettyPhoto
-// jQuery Flot
-//= require admin/excanvas.min
-//= require admin/jquery.flot
-//= require admin/jquery.flot.resize
-//= require admin/jquery.flot.pie
-//= require admin/jquery.flot.stack
-// jQuery Notification - Noty
-//= require admin/jquery.noty
-//= require admin/themes/default
-//= require admin/layouts/bottom
-//= require admin/layouts/topRight
-//= require admin/layouts/top
-
-//= require admin/sparklines
-//= require admin/jquery.cleditor.min
-//= require admin/bootstrap-datetimepicker.min
-//= require admin/jquery.uniform.min
-//= require admin/jquery.toggle.buttons
-//= require admin/filter
-//= require admin/charts
-
-//= require jquery
-//= require jquery_ujs
-//= require jquery.ui.all
-//= require jquery-ui-sliderAccess
-//= require jquery-ui-timepicker-addon
-//= require jquery-ui-timepicker-zh-CN
-//= require ckeditor/init
-//= require jquery.readyselector
+//= require lib/jquery.readyselector
 
 // later use require_tree ./admin
 //= require admin/notices
 //= require admin/page_ads
 //= require admin/shop_recommendations
+
 
 
