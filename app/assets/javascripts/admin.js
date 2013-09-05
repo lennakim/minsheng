0b7fdbdcd5b575131c234b47b4a4d957fec6be0b
zongyannan <zongyannan@shengtianbo.com>
2013-09-05 19:01:48 +0800

    后台修改

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,12 +1,12 @@
 //= require admin/jquery
+//= require jquery_ujs
 //= require lib/bootstrap
-//= require admin/jquery-ui-1.9.2.custom.min
 //= require lib/jquery.readyselector
+//= require lib/jquery-ui.min
 
 // later use require_tree ./admin
 //= require admin/notices
 //= require admin/page_ads
 //= require admin/shop_recommendations
-
 
 
--- app/assets/javascripts/admin/page_ads.js
+++ app/assets/javascripts/admin/page_ads.js
@@ -1,4 +1,4 @@
-$('body.page_ads_new, body.page_ads_edit, body.page_ads_create, body.page_ads_update').ready(function(){
+$('body.page_ads_new, body.page_ads_edit').ready(function(){
   $('.datetime').datetimepicker({
     timeFormat: 'HH:mm:ss',
     addSliderAccess: true,
