5f65213dfaec6a055ed704134bf08d3db8e9a19c
zongyannan <zongyannan@shengtianbo.com>
2013-09-06 16:51:49 +0800

    modify datepicker

--- app/assets/javascripts/admin/shop_recommendations.js
+++ app/assets/javascripts/admin/shop_recommendations.js
@@ -1,7 +1,3 @@
 $('body.shop_recommendations_new, body.shop_recommendations_edit, body.shop_recommendations_create, body.shop_recommendations_update').ready(function(){
-  $('#shop_recommendation_start_time, #shop_recommendation_end_time').datetimepicker({
-    timeFormat: 'HH:mm:ss',
-    addSliderAccess: true,
-    sliderAccessArgs: { touchonly: false}
-  });
+  $('#shop_recommendation_start_time, #shop_recommendation_end_time').datetimepicker();
 });
