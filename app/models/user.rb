0e67875bafd2a1240bb29e9f3f7108a55c608ac9
shooter <shooter.hero@gmail.com>
2013-09-27 16:31:58 +0800

    add gem client_side_validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,9 +16,12 @@ class User < ActiveRecord::Base
 
   # constants definition ......................................................
   # validations ...............................................................
-  validates :name, :uniqueness => { :case_sensitive => false }
+  validates :name, :presence => { :message =>"请填写用户名" }
+  validates :name, :uniqueness => { :case_sensitive => false, :message =>"用户已注册"}
 
   validates :mobile, :uniqueness => true, :numericality => { :only_integer => true }, :allow_blank => true, :allow_nil => true
+  validates :password, :presence => { :message => "请输入密码" }
+  validates_confirmation_of :password, :message => "重复密码"
   # callbacks .................................................................
   # scopes ....................................................................
 
