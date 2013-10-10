cacabd3e7392c6c1685d2527c62a99d6445db456
zongyannan <zongyannan@shengtianbo.com>
2013-10-10 18:19:08 +0800

    shop images add animate

--- app/assets/javascripts/mobile/shop_images.js
+++ app/assets/javascripts/mobile/shop_images.js
@@ -1,18 +1,43 @@
 $(document).ready(function(){
+  removeUselessCenterPartial = function(){
+    if($('._center_partial').length != 1){
+      $($('._image_partial').find('._center_partial:first')).remove()
+    }
+  };
+
   $('._image_partial').on('click', '#previous_image', function(e){
     e.preventDefault();
-    if($(this).data('is-previous')){
+    $this = $(this)
+    if($this.data('is-previous')){
       url = $(this).data('url');
-      $.get(url, {}, function(data){
-        $('._image_partial').html(data.html);
+      $.get(url, {}, function(result){
+        $current_image_box = $this.parents('._image_partial');
+        $current_center_partial = $current_image_box.find('._center_partial:first');
+        $new_html = $('<div></div>');
+        $new_html.html(result.html);
+        $new_center_partial = $new_html.find('._center_partial');
+        $new_center_partial.insertAfter($current_center_partial);
+        $current_center_partial.animate({top: '-300px', opacity: 0}, {complete: setTimeout(removeUselessCenterPartial, 200)}).
+           next('._center_partial').css({top: '600px', opacity: 0, position: 'relative'}).
+           animate({top:'0', opacity: 1}, {complete: setTimeout(removeUselessCenterPartial, 200)});
       });
+    return false;
     }
   }).on('click', '#next_image', function(e){
     e.preventDefault();
+    $this = $(this)
     if($(this).data('is-next')){
       url = $(this).data('url');
-      $.get(url, {}, function(data){
-        $('._image_partial').html(data.html);
+      $.get(url, {}, function(result){
+        $current_image_box = $this.parents('._image_partial');
+        $current_center_partial = $current_image_box.find('._center_partial:first');
+        $new_html = $('<div></div>');
+        $new_html.html(result.html);
+        $new_center_partial = $new_html.find('._center_partial');
+        $new_center_partial.insertAfter($current_center_partial);
+        $current_center_partial.animate({bottom: '-300px', opacity: 0}, {complete: setTimeout(removeUselessCenterPartial, 200)}).
+           next('._center_partial').css({bottom: '600px', opacity: 0, position: 'relative'}).
+           animate({bottom:'0', opacity: 1}, {complete: setTimeout(removeUselessCenterPartial, 200)});
       });
     }
   });
